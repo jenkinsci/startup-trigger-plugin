2208852ffe19990be40db3bf03df969065c37b63
Luke Robertson <lrobertson39@hotmail.com>
2014-10-01 16:10:40 +0100

    Correct StartupCause parameter passing

--- src/main/java/org/jvnet/hudson/plugins/triggers/startup/HudsonStartupCause.java
+++ src/main/java/org/jvnet/hudson/plugins/triggers/startup/HudsonStartupCause.java
@@ -4,8 +4,15 @@ import hudson.model.Cause;
 
 public class HudsonStartupCause extends Cause {
 
+    Node node;
+
+    public HudsonStartupCause(Node node)
+    {
+        this.node = node
+    }
+
     @Override
-    public String getShortDescription(Node node) {
+    public String getShortDescription() {
         return "Started due to the start of the node " + node.getDisplayName();
     }
 }
