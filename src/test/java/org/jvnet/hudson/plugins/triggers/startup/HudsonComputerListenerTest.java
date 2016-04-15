f16647be01c625c6b762544426ddcfa6b2d4a32c
etiennebec <etienne.bec@non.schneider-electric.com>
2016-04-15 14:34:28 +0200

    Add tests and update required core version to 1.480
    I wrote three tests cases which check if the job is triggered when it's at the root level / in a folder / in a subfolder.
    I had to bump the required core version to 1.480 to use Cloudbees Folder in my tests.
    
    Change-Id: I76f2fe559901e08f2218d6ff35721902bb4d49d3

---
+++ src/test/java/org/jvnet/hudson/plugins/triggers/startup/HudsonComputerListenerTest.java
@@ -0,0 +1,79 @@
+package org.jvnet.hudson.plugins.triggers.startup;
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, Schneider Electric
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import com.cloudbees.hudson.plugins.folder.Folder;
+import hudson.model.FreeStyleProject;
+import org.jvnet.hudson.test.HudsonTestCase;
+
+
+public class HudsonComputerListenerTest extends HudsonTestCase {
+    public void testRootJob() throws Exception {
+        // Create job with startup trigger
+        FreeStyleProject job = createFreeStyleProject("job");
+        job.addTrigger(new HudsonStartupTrigger("slave0", null));
+
+        // Create slave which node name will be slave0
+        createOnlineSlave();
+
+        // Wait for the completion of the build
+        waitUntilNoActivity();
+
+        assertTrue(job.getLastSuccessfulBuild().number == 1);
+    }
+
+    public void testFolderJob() throws Exception {
+        // Create one level folder and a job with startup trigger
+        Folder folder = this.hudson.createProject(Folder.class, "folder1");
+        FreeStyleProject job = folder.createProject(FreeStyleProject.class, "job");
+        job.addTrigger(new HudsonStartupTrigger("slave0", null));
+
+        // Create slave which node name will be slave0
+        createOnlineSlave();
+
+        // Wait for the completion of the build
+        waitUntilNoActivity();
+
+        assertTrue(job.getLastSuccessfulBuild().number == 1);
+    }
+
+    public void testSubFolderJob() throws Exception {
+        // Create two level folder and a job with startup trigger
+        Folder folder1 = this.hudson.createProject(Folder.class, "folder1");
+        Folder folder2 = folder1.createProject(Folder.class, "folder2");
+
+        FreeStyleProject job = folder2.createProject(FreeStyleProject.class, "job");
+        job.addTrigger(new HudsonStartupTrigger("slave0", null));
+
+
+        // Create slave which node name will be slave0
+        createOnlineSlave();
+
+        // Wait for the completion of the build
+        waitUntilNoActivity();
+
+        assertTrue(job.getLastSuccessfulBuild().number == 1);
+    }
+}
